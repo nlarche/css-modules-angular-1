34022483ce28eafb6da1c5faf4ec3f7f29900e2d
nilar <nicolas.larche@rca.fr>
2016-04-12 11:50:55 +0200

    fix bug

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import angular from 'angular'
 import styles from './index.css'
 
 var template = `
-        <div class={{::$ctrl.styles.content}}>                   
+        <div class="{{::$ctrl.styles.content}}">                   
             <div class="{{$ctrl.styles.pancakes}}">pancakes</div>
             <div ng-class="$ctrl.styles.bacon">bacon</div>
         </div>`;
